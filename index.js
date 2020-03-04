c8f9cf24279377cc71fc53607344224369478924
yamacho <yamacho@users.noreply.github.com>
2020-03-04 13:30:01 +0900

    GET でアクセスした際にアンケートフォームを表示するように変更

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const server = http.createServer((req, res) => {
   const now = new Date();
   console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
 
   switch (req.method) {
     case 'GET':
-      res.write('GET ' + req.url);
+      const fs = require('fs');
+      const rs = fs.createReadStream('./form.html');
+      rs.pipe(res);
       break;
     case 'POST':
       res.write('POST ' + req.url);
@@ -18,12 +20,12 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         console.info('[' + now + '] Data posted: ' + rawData);
+        res.end();
       });
       break;
     default:
       break;
   }
-  res.end();
 }).on('error', (e) => {
   console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
